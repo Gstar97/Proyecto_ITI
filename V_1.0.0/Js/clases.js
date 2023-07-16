6224748ea9231943a7d45d6589777256cc31e5e6
Geronimo <gero9759@gmail.com>
2023-07-16 19:06:00 -0300

    *Se trabajo en la logica del registro *Se agrego nuevo menu con bootstrap

--- V_1.0.0/Js/clases.js
+++ V_1.0.0/Js/clases.js
@@ -1,5 +1,6 @@
 class Registro {
-    constructor(unId, unaCedula, unNombre, unApellido, unCelular, unEmail, unaClave, unBarrio, unaDireccion, unaEsquina) {
+    constructor(unId, unaCedula, unNombre, unApellido, unCelular, unEmail, unaClave,
+      unaConfirmacionClave, unBarrio, unaDireccion, unaEsquina) {
       this.Id = unId;
       this.cedula = unaCedula;
       this.nombre = unNombre;
@@ -7,6 +8,7 @@ class Registro {
       this.celular = unCelular;
       this.email = unEmail;
       this.clave = unaClave;
+      this.confirmacionClave = unaConfirmacionClave
       this.barrio = unBarrio;
       this.direccion = unaDireccion;
       this.esquina = unaEsquina;
--- V_1.0.0/Js/registro.js
+++ V_1.0.0/Js/registro.js
@@ -1,38 +1,74 @@
 window.addEventListener("load", inicio)
 
 function inicio() {
-    
+
     document.querySelector("#btnRegistro").addEventListener("click", tomarValores);
     cargarComboBarrios();
 }
 
+let sistema = new Sistema()
+function cargarComboBarrios() {
+    for (let i = 0; i < sistema.barrios.length; i++) {
+        const unBarrio = sistema.barrios[i];
+
+        document.querySelector("#slcBarrioRegistro").innerHTML += `
+    <option value = ${unBarrio.id}>${unBarrio.barrio}</option>`
+    }
+}
+
 function tomarValores() {
-    let cedulaRegistro = Number(document.querySelector("#txtCedulaRegistro").value);
+    let cedulaRegistro = document.querySelector("#txtCedulaRegistro").value;
     let nombreRegistro = document.querySelector("#txtNombreRegistro").value;
     let apellidoRegistro = document.querySelector("#txtApellidoRegistro").value;
     let emailRegistro = document.querySelector("#txtEmailRegistro").value;
-    let telefonoRegistro = Number(document.querySelector("#txtTelefonoRegistro").value);
+    let telefonoRegistro = document.querySelector("#txtTelefonoRegistro").value;
     let direccionRegistro = document.querySelector("#txtDireccionRegistro").value;
     let barrioRegistro = document.querySelector("#slcBarrioRegistro").value;
     let claveRegistro = document.querySelector("#txtClaveRegistro").value;
     let confirmacionClaveRegistro = document.querySelector("#txtConfirmarClaveRegistro").value;
     let tipoClienteRegistro = document.querySelector("#slcTipoClienteRegistro").value;
 
-    
-    
+    agregarRegistro(cedulaRegistro, nombreRegistro, apellidoRegistro, telefonoRegistro,
+        emailRegistro, claveRegistro, confirmacionClaveRegistro, barrioRegistro, direccionRegistro)
 }
 
-let sistema = new Sistema()
-function cargarComboBarrios() {
-   for (let i = 0; i < sistema.barrios.length; i++) {
-    const unBarrio = sistema.barrios[i];
-    
-    document.querySelector("#slcBarrioRegistro").innerHTML +=`
-    <option value = ${unBarrio.id}>${unBarrio.barrio}</option>`
-   }
-   }
+let idRegistro = 0;
+function agregarRegistro(unaCedulaRegistro, unNombreRegistro, unApellidoRegistro, unTelefonoRegistro,
+    unEmailRegistro, unaClaveRegistro, unaConfirmacionRegistro, unBarrioRegistro, unaDireccionRegistro) {
+        document.querySelector("#pMostrar").innerHTML=""
+    if (unaCedulaRegistro === "" || unNombreRegistro === "" || unApellidoRegistro === "" ||
+        unTelefonoRegistro === "" || unEmailRegistro === "" || unaClaveRegistro === "" ||
+        unaConfirmacionRegistro === "" || unBarrioRegistro === "" || unaDireccionRegistro === "") {
+        document.querySelector("#pMostrar").innerHTML = `Por favor revise que no haya campos vacios o opciones sin seleccionar`
+    } else if (unaCedulaRegistro.length < 8){
+        document.querySelector("#pMostrar").innerHTML=`Formato de cedula invalido`
+    } else if (unTelefonoRegistro.length < 9){
+        document.querySelector("#pMostrar").innerHTML = `Formato de telefono invalido`
+    } else if (isNaN(unaCedulaRegistro) && isNaN(unTelefonoRegistro)) {
+        document.querySelector("#pMostrar").innerHTML = `Formato de cedula y telefono invalido`
+    } else if (isNaN(unaCedulaRegistro)) {
+        document.querySelector("#pMostrar").innerHTML = `Formato de cedula invalido`
+    } else if (isNaN(unTelefonoRegistro)) {
+        document.querySelector("#pMostrar").innerHTML = `Formato de telefono invalido`
+    }
+    else {
+        idRegistro++
+        let registro = new Registro(idRegistro, unaCedulaRegistro, unNombreRegistro, unApellidoRegistro, unTelefonoRegistro,
+            unEmailRegistro, unaClaveRegistro, unaConfirmacionRegistro, unBarrioRegistro, unaDireccionRegistro)
+        sistema.registros.push(registro)
+        document.querySelector("#pMostrar").innerHTML = `Registro exitoso`
+    }
+}
 
-    
+
+
+
+
+
+
+
+
+
 
 
 
