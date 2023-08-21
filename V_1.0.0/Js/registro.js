b9f1d2d27b5c39fef462492f55aa97fa255c32f4
Gstar97 <gero9759@gmail.com>
2023-08-21 14:13:18 -0300

    Se arreglaron las validaciones usando expresiones regulares 	# Please enter the commit message for your changes. Lines starting

--- V_1.0.0/Js/registro.js
+++ V_1.0.0/Js/registro.js
@@ -6,6 +6,10 @@ function inicio() {
     cargarComboBarrios();
 }
 
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const cedulaRegex = /^[0-9]+$/;
+const claveRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+
 let sistema = new Sistema()
 function cargarComboBarrios() {
     for (let i = 0; i < sistema.barrios.length; i++) {
@@ -41,18 +45,22 @@ function validarRegistro(unaCedulaRegistro, unNombreRegistro, unApellidoRegistro
         unTelefonoRegistro === "" || unEmailRegistro === "" || unaClaveRegistro === "" ||
         unaConfirmacionRegistro === "" || unBarrioRegistro === "" || unaDireccionRegistro === "") {
         document.querySelector("#pMostrar").innerHTML = `Por favor revise que no haya campos vacios o opciones sin seleccionar`
-    } else if (unaCedulaRegistro.length < 8){
+    } else if (unaCedulaRegistro.length < 8 || !cedulaRegex.test(unaCedulaRegistro)){
         document.querySelector("#pMostrar").innerHTML=`Formato de cedula invalido`
     } else if (unTelefonoRegistro.length < 9){
         document.querySelector("#pMostrar").innerHTML = `Formato de telefono invalido`
     } else if (isNaN(unaCedulaRegistro) && isNaN(unTelefonoRegistro)) {
         document.querySelector("#pMostrar").innerHTML = `Formato de cedula y telefono invalido`
-    } else if (isNaN(unaCedulaRegistro)) {
-        document.querySelector("#pMostrar").innerHTML = `Formato de cedula invalido`
     } else if (isNaN(unTelefonoRegistro)) {
         document.querySelector("#pMostrar").innerHTML = `Formato de telefono invalido`
     } else if (sistema.verificarRegistroRepetido(unaCedulaRegistro,unEmailRegistro)){
         document.querySelector("#pMostrar").innerHTML = `El usuario ingresado ya existe`
+    } else if (!emailRegex.test(unEmailRegistro)){
+        document.querySelector("#pMostrar").innerHTML = `Formato de correo invalido`
+    } else if (unaClaveRegistro !== unaConfirmacionRegistro){
+        document.querySelector("#pMostrar").innerHTML = `Las contraseñas no coinciden`
+    } else if (!claveRegex.test(unaClaveRegistro)){
+        document.querySelector("#pMostrar").innerHTML = `La contraseña debe de tener una mayuscula, un numero y un largo de 8 caracteres como minimo.`
     } else {
         document.querySelector("#pMostrar").innerHTML = `Registro exitoso`
     }
