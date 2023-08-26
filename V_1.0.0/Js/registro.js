ae95341cb3e22c96b7645ccfdb5b6c0d7e4af285
Gstar97 <gero9759@gmail.com>
2023-08-26 12:28:21 -0300

    Se añado el fetch

--- V_1.0.0/Js/registro.js
+++ V_1.0.0/Js/registro.js
@@ -1,79 +1,20 @@
-window.addEventListener("load", inicio)
+var formulario = document.getElementById('registro');
 
-function inicio() {
+formulario.addEventListener('submit', function(e){
+e.preventDefault();
 
-    document.querySelector("#btnRegistro").addEventListener("click", tomarValores);
-    cargarComboBarrios();
-}
+    var datos = new FormData(formulario);
 
-const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-const cedulaRegex = /^[0-9]+$/;
-const claveRegex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+        fetch('Php/registro.php',{
+            method: 'POST',
+            body: datos
+        })
+            .then(res => res.json())
+            .then(data => {
 
-let sistema = new Sistema()
-function cargarComboBarrios() {
-    for (let i = 0; i < sistema.barrios.length; i++) {
-        const unBarrio = sistema.barrios[i];
+                document.querySelector("#pMostrar").innerHTML="Registro exitoso";
+                
+            })
 
-        document.querySelector("#slcBarrioRegistro").innerHTML += `
-    <option value = ${unBarrio.id}>${unBarrio.barrio}</option>`
-    }
-}
-
-function tomarValores() {
-    let cedulaRegistro = document.querySelector("#txtCedulaRegistro").value;
-    let nombreRegistro = document.querySelector("#txtNombreRegistro").value;
-    let apellidoRegistro = document.querySelector("#txtApellidoRegistro").value;
-    let emailRegistro = document.querySelector("#txtEmailRegistro").value;
-    let telefonoRegistro = document.querySelector("#txtTelefonoRegistro").value;
-    let direccionRegistro = document.querySelector("#txtDireccionRegistro").value;
-    let barrioRegistro = document.querySelector("#slcBarrioRegistro").value;
-    let claveRegistro = document.querySelector("#txtClaveRegistro").value;
-    let confirmacionClaveRegistro = document.querySelector("#txtConfirmarClaveRegistro").value;
-    let tipoClienteRegistro = document.querySelector("#slcTipoClienteRegistro").value;
-    
-    validarRegistro(cedulaRegistro, nombreRegistro, apellidoRegistro, telefonoRegistro,
-        emailRegistro, claveRegistro, confirmacionClaveRegistro, barrioRegistro, direccionRegistro)
-    }
-
-
-
-function validarRegistro(unaCedulaRegistro, unNombreRegistro, unApellidoRegistro, unTelefonoRegistro,
-    unEmailRegistro, unaClaveRegistro, unaConfirmacionRegistro, unBarrioRegistro, unaDireccionRegistro) {
-        document.querySelector("#pMostrar").innerHTML=""
-    if (unaCedulaRegistro === "" || unNombreRegistro === "" || unApellidoRegistro === "" ||
-        unTelefonoRegistro === "" || unEmailRegistro === "" || unaClaveRegistro === "" ||
-        unaConfirmacionRegistro === "" || unBarrioRegistro === "" || unaDireccionRegistro === "") {
-        document.querySelector("#pMostrar").innerHTML = `Por favor revise que no haya campos vacios o opciones sin seleccionar`
-    } else if (unaCedulaRegistro.length < 8 || !cedulaRegex.test(unaCedulaRegistro)){
-        document.querySelector("#pMostrar").innerHTML=`Formato de cedula invalido`
-    } else if (unTelefonoRegistro.length < 9){
-        document.querySelector("#pMostrar").innerHTML = `Formato de telefono invalido`
-    } else if (isNaN(unaCedulaRegistro) && isNaN(unTelefonoRegistro)) {
-        document.querySelector("#pMostrar").innerHTML = `Formato de cedula y telefono invalido`
-    } else if (isNaN(unTelefonoRegistro)) {
-        document.querySelector("#pMostrar").innerHTML = `Formato de telefono invalido`
-    } else if (sistema.verificarRegistroRepetido(unaCedulaRegistro,unEmailRegistro)){
-        document.querySelector("#pMostrar").innerHTML = `El usuario ingresado ya existe`
-    } else if (!emailRegex.test(unEmailRegistro)){
-        document.querySelector("#pMostrar").innerHTML = `Formato de correo invalido`
-    } else if (unaClaveRegistro !== unaConfirmacionRegistro){
-        document.querySelector("#pMostrar").innerHTML = `Las contraseñas no coinciden`
-    } else if (!claveRegex.test(unaClaveRegistro)){
-        document.querySelector("#pMostrar").innerHTML = `La contraseña debe de tener una mayuscula, un numero y un largo de 8 caracteres como minimo.`
-    } else {
-        document.querySelector("#pMostrar").innerHTML = `Registro exitoso`
-    }
-}
-        
-
-
-
-
-
-
-
-
-
-
+    })
 
--- V_1.0.0/Php/registro.php
+++ V_1.0.0/Php/registro.php
@@ -1,20 +1,8 @@
 <?php
-require "clases.php";
-$cedulaRegistro = $_POST["txtCedulaRegistro"];
-$nombreRegistro = $_POST["txtNombreRegistro"];
-$apellidoRegistro = $_POST["txtApellidoRegistro"];
-$emailRegistro = $_POST["txtEmailRegistro"];
-$telefonoRegistro = $_POST["txtTelefonoRegistro"];
-$direccionRegistro = $_POST["txtDireccionRegistro"];
-$barrioRegistro = $_POST["slcBarrioRegistro"];
-$claveRegistro = $_POST["txtClaveRegistro"];
-$confirmacionClaveRegistro = $_POST["txtConfirmarClaveRegistro"];
-$tipoClienteRegistro = $_POST["slcTipoClienteRegistro"];
 
-$Registro = new registro (cedulaRegistro, nombreRegistro, apellidoRegistro, emailRegistro, telefonoRegistro, direccionRegistro, barrioRegistro,
-                         txtClaveRegistro, txtConfirmarClaveRegistro, slcTipoClienteRegistro);
+$Ci = $_POST["txtCedulaRegistro"];
 
-echo $Registro -> getCedula();
+echo json_encode("Registro exitoso");
 
 
 ?>
